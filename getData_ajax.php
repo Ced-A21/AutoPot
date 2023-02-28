c10c273d55f8c9643e1a2c6b4fb00b5425e22329
SIMON PHILIP .ASI�O <spasio@live.mcl.edu.ph>
2023-03-01 00:37:18 +0800

    simon

--- getData_ajax.php
+++ getData_ajax.php
@@ -25,11 +25,11 @@
             while($row=mysqli_fetch_array($check)) {
 
                 if ($row['temp'] < '30'){
-                    $imgURL = 'Images/sample1.png';
+                    $imgURL = 'Images/sample4.png';
                 }
 
                 echo "
-                <div class='card-columns' id='dataCol' style='display: flex; justify-content: center; flex-direction: column; padding-left:50vh; padding-bottom:25vh'>
+                <div class='card-columns' id='dataCol' style='display: flex; justify-content: center; flex-direction: column; padding-left:60vh; padding-bottom:25vh; background-image:url($imgURL);'>
                     <div class='card' style='max-width: 12rem;' id='tempCol'>
                         <div class='card-body' id='card1'>
                         <p class='card-title' align='left'>Temperature<p>
