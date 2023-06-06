28cd75563b6c7cb8bc2cab28fa3b9c407f5d85fe
jshalstr <joshaleisterv@gmail.com>
2023-06-06 08:39:22 +0800

    updated bootstrap

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 
     <head>
 
-        <link href="../css/bootstrap.min.css" rel="stylesheet" />
+        <!-- <link href="../css/bootstrap.min.css" rel="stylesheet" /> -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet" />
         <link href="autopotstyles.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;1,200&display=swap" rel="stylesheet"/>
@@ -19,6 +20,7 @@
         </style>
 
         <script type="text/javascript"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <script type="text/javascript" src="js/jquery-ui.min.js" ></script>
         <script type="text/javascript" src="js/jquery-1.6.3.min.js" ></script>
         <script type="text/javascript" src="js/jquery-ui.min.js" ></script>
